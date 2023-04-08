765df80b62c04e411152d92641cd9d2677beb4f6
Ishan Malviya <ishan.malviya@gmail.com>
2023-04-08 20:16:29 +0530

    Sets up timer to load the landing page from splash screen

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+import 'dart:async';
+
+import '../landing_screen/landing_screen.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final timer = Timer(
+      const Duration(seconds: 5),
+      () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const LandingScreen(),
+          ),
+        );
+      },
+    );
     return Scaffold(
       body: Center(
         child: Padding(
