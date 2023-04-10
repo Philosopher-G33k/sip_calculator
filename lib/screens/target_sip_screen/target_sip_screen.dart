32a6b064a8757e001ee65359ac3419f8d5ae0514
Ishan Malviya <ishan.malviya@gmail.com>
2023-04-11 00:35:08 +0530

    Adds the logic to show app store review screen and shared preference to the target SIP screen

--- lib/screens/target_sip_screen/target_sip_screen.dart
+++ lib/screens/target_sip_screen/target_sip_screen.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import '../reusable/sip_form.dart';
 import '../reusable/sip_maturity.dart';
 
+import '../../utils/utils.dart';
+import 'package:in_app_review/in_app_review.dart';
+
 class TargetSipScreen extends StatefulWidget {
   const TargetSipScreen({super.key});
 
@@ -18,14 +21,22 @@ class _TargetSipScreenState extends State<TargetSipScreen> {
   var initialInvestmentAmount = 0;
   var estimatedReturns = 0;
 
+  final InAppReview _inAppReview = InAppReview.instance;
+
   void resetHanlder() {
     setState(() {
       isSIPCalculationReady = false;
     });
   }
 
+  void _requestReview() async {
+    if (await _inAppReview.isAvailable()) {
+      _inAppReview.requestReview();
+    }
+  }
+
   void calculateMonthlySIP(
-      double targetSavings, double duration, double returnPercentage) {
+      double targetSavings, double duration, double returnPercentage) async {
     final convertedPercentage = returnPercentage / 1200;
     final convertedDuration = duration * 12;
     final monthlyInvestment = (targetSavings) /
@@ -38,6 +49,16 @@ class _TargetSipScreenState extends State<TargetSipScreen> {
       estimatedReturns = (targetSavings - initialInvestmentAmount).toInt();
       isSIPCalculationReady = true;
     });
+
+    int counter = await Utils().readCounter();
+    if (counter >= 5) {
+      // Show the prompt
+      // Reset the counter
+      _requestReview();
+      await Utils().resetCounter();
+    } else {
+      await Utils().incrementCounter();
+    }
   }
 
   @override
