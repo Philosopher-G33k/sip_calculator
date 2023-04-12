fbefe2da1d2d7845d38285318f67fe71d3e5cb71
Ishan Malviya <ishan.malviya@gmail.com>
2023-04-13 02:31:25 +0530

    Adds logic to show ad on Monthly sip screen

--- lib/screens/monthly_sip_screen/monthly_sip_screen.dart
+++ lib/screens/monthly_sip_screen/monthly_sip_screen.dart
@@ -7,6 +7,10 @@ import '../../utils/utils.dart';
 
 import 'package:in_app_review/in_app_review.dart';
 
+// TODO: Import google_mobile_ads.dart
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import '../../utils/ad_helper.dart';
+
 class MonthlySipScreen extends StatefulWidget {
   const MonthlySipScreen({super.key});
 
@@ -20,6 +24,8 @@ class _MonthlySipScreenState extends State<MonthlySipScreen> {
   var sipMaturityValue = 0;
   var initialInvestmentAmount = 0;
   var estimatedReturns = 0;
+
+  BannerAd? _bannerAd;
 
   final InAppReview _inAppReview = InAppReview.instance;
 
@@ -61,6 +67,29 @@ class _MonthlySipScreenState extends State<MonthlySipScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    // TODO: Load a banner ad
+    BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          ad.dispose();
+        },
+      ),
+    ).load();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -81,6 +110,15 @@ class _MonthlySipScreenState extends State<MonthlySipScreen> {
                     estimatedReturns: estimatedReturns.toString(),
                     initialInvestmentAmount:
                         initialInvestmentAmount.toString()),
+              if (_bannerAd != null)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    width: _bannerAd!.size.width.toDouble(),
+                    height: _bannerAd!.size.height.toDouble(),
+                    child: AdWidget(ad: _bannerAd!),
+                  ),
+                ),
             ],
           ),
         ),
--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -1,9 +1,19 @@
 import 'dart:io';
 
 class AdHelper {
+  // static String get bannerAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return 'ca-app-pub-8891336915917777/6639842573';
+  //   } else if (Platform.isIOS) {
+  //     return 'ca-app-pub-3940256099942544/2934735716';
+  //   } else {
+  //     throw new UnsupportedError('Unsupported platform');
+  //   }
+  // }
+
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-8891336915917777/6639842573';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
