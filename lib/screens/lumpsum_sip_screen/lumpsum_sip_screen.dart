63cb401d35456013acb5d599b1c3740ff6451214
Ishan Malviya <ishan.malviya@gmail.com>
2023-04-14 00:54:47 +0530

    Adds the ads showing logic to lumpsum screen

--- lib/screens/lumpsum_sip_screen/lumpsum_sip_screen.dart
+++ lib/screens/lumpsum_sip_screen/lumpsum_sip_screen.dart
@@ -7,6 +7,10 @@ import '../reusable/sip_maturity.dart';
 import '../../utils/utils.dart';
 import 'package:in_app_review/in_app_review.dart';
 
+// TODO: Import google_mobile_ads.dart
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import '../../utils/ad_helper.dart';
+
 class LumpsumSipScreen extends StatefulWidget {
   const LumpsumSipScreen({super.key});
 
@@ -20,6 +24,8 @@ class _LumpsumSipScreenState extends State<LumpsumSipScreen> {
   var sipMaturityValue = 0;
   var initialInvestmentAmount = 0;
   var estimatedReturns = 0;
+
+  BannerAd? _bannerAd;
 
   final InAppReview _inAppReview = InAppReview.instance;
 
@@ -61,6 +67,29 @@ class _LumpsumSipScreenState extends State<LumpsumSipScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    // TODO: Load a banner ad
+    BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _bannerAd = ad as BannerAd;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          ad.dispose();
+        },
+      ),
+    ).load();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -75,6 +104,16 @@ class _LumpsumSipScreenState extends State<LumpsumSipScreen> {
                 resetHandler: resetHanlder,
                 investmentFieldTitle: "Lumpsum Investment",
               ),
+              if (_bannerAd != null)
+                Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+                    width: _bannerAd!.size.width.toDouble(),
+                    height: _bannerAd!.size.height.toDouble(),
+                    child: AdWidget(ad: _bannerAd!),
+                  ),
+                ),
               if (isSIPCalculationReady)
                 SipMaturity(
                     sipMaturityValue: sipMaturityValue.toString(),
