12650ccd859d2d7a52888c646d20b023ce6a680e
Ishan Malviya <ishan.malviya@gmail.com>
2023-04-26 18:11:08 +0530

    Adds the prod Ad ID and Banner ID for iOS

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -15,7 +15,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return 'ca-app-pub-8891336915917777/8619449712';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
